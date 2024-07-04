f343e47e63093690f6f50ff301fc0a07bb62cc73
Luis Baquedano <lfbcordova@gmail.com>
2024-07-04 10:50:54 -0500

    new file eslint.config.msj, modified file index.js, etc

---
+++ eslint/eslint-npm/index.js
@@ -0,0 +1,24 @@
+//este es archivo que vamosa utilizar
+
+const nombre = "Fernando" //eslint-disable-line
+
+
+
+ 
+
+ 
+
+
+const nuevoString = 'esto es nuevo string' // eslint-disable-line
+
+ 
+
+  const string2 = 'mas string'
+  string2
+
+  console.log('hola');
+
+  const persona3 = 'juan'
+  persona3
+
+  console.log(4);
